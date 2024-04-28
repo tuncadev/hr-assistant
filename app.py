6d9703ee072e3fb6e4f7e34e4a782f9155ce170c
tuncadev <tunca.development@gmail.com>
2024-04-28 23:29:19 +0300

    started

--- app.py
+++ app.py
@@ -1,31 +1,219 @@
 import os
+import uuid
+
 import streamlit as st
 from crewai import Agent, Task
 from langchain_openai import ChatOpenAI
 
+from tools.getvacancies import GetVacanciesData
+from tools.read_file import ReadFileContents
+
 openai_api = os.getenv("OPENAI_API_KEY")
 os.environ["OPENAI_API_KEY"] = str(openai_api)
+open_llm = ChatOpenAI(
+    model_name="gpt-4-turbo",
+    temperature=0.1
+)
 
-agent = Agent(
-    role='A Friend',
-    goal='Just answer the question',
-    backstory='You are an experienced psychologist, and your goal is to cheer people up',
+# App functions start #
+
+
+# Check if all fields are filled
+def are_all_fields_filled(responses):
+    for key, value in responses.items():
+        if not value:
+            return False
+    return True
+
+
+def get_temp_path():
+    if 'temp_path' not in st.session_state:
+        temp_name = str(uuid.uuid4())
+        temp_path = f"temp/{temp_name}"
+        os.makedirs(temp_path, exist_ok=True)
+        st.session_state['temp_path'] = temp_path
+    return st.session_state['temp_path']
+
+
+def check_uploaded_file(uploaded_file):
+    file_name = uploaded_file.name
+    file_content = uploaded_file.getvalue()
+    readfile = ReadFileContents(file_name, file_content)
+    if file_name.endswith('.doc'):
+        cv_content = readfile.read_doc_file()
+    elif file_name.endswith('.docx'):
+        cv_content = readfile.read_docx_file()
+    elif file_name.endswith('.txt'):
+        cv_content = readfile.read_txt_file()
+    elif file_name.endswith('.pdf'):
+        cv_content = readfile.read_pdf_file()
+    else:
+        cv_content = "Unsupported file type"
+    return cv_content
+# App functions end #
+
+
+# Agents #
+##############################################
+def cv_analyst():
+    return Agent(
+        role='Cv Analyst',
+        goal='Analyze the given Resume context and rewrite it in the required format.',
+        backstory="""
+        You are an experienced professional Senior Human Resources Manager. 
+        You have 20+ years of experience in job interviews and resume reviewing. 
+        You can look into a resume and tell where the gaps are between the vacancy requirements and the resume content.',
+        """,
+        allow_delegation=False,
+        verbose=True,
+        llm=open_llm
+    )
+
+
+##############################################
+hr_interviewer = Agent(
+    role='Human Resources Manager',
+    goal="""
+    Read the analysis report of a resume for a certain job vacancy. 
+    Create minimum 6 and maximum of 10 questions to ask to the candidate to gather more information (if needed).
+    """,
+    backstory="""
+    You are an experienced Senior Human Resources Manager. 
+    You have 25+ years of experience in job interviews and resume reviewing. 
+    You can read the report of a resume and create some questions to ask to the candidate for further clarification of vacancy satisfactory analysis.
+    """,
     allow_delegation=False,
     verbose=True,
-    llm=ChatOpenAI(
-        model_name="gpt-3.5-turbo",
-        temperature=0.3
-    )
+    llm=open_llm
 )
 
 
-prompt = st.chat_input("Say something")
-if prompt:
-    task = Task(
-        description=f"{prompt}",
-        expected_output=f"Answer the question",
-        agent=agent,
+##############################################
+hr_manager = Agent(
+    role='Senior Human Resources Manager',
+    goal="""
+           * Read vacancy details.
+           * Read the first analysis report of a resume.
+           * Read the resume details. 
+           * Read the additional questions and answers of the candidate from the interview.
+           * Write a final report about the candidate. How suitable he is for the position. 
+           * Pay attention to vacancy requirements and how well the candidate fills the requirements.
+           """,
+    backstory="""
+           You are an extremely professional, very experienced  Senior Human Resources Manager. 
+           You have 25+ years of experience in job interviews and resume reviewing. 
+           You can read the report of a resume and results of an interview and write a professional report about the candidate.
+           You do not improvise or imagine. You work with facts.
+           """,
+    allow_delegation=False,
+    verbose=True,
+    llm=open_llm
+)
+
+
+##############################################
+def hr_dep_manager():
+    return Agent(
+        role='Human Resources General Manager',
+        goal="""
+            * Read vacancy details.
+            * Read the first analysis report of a resume.
+            * Read the resume details. 
+            * Read the additional questions and answers of the candidate from the interview.
+            * Read the final report about the candidate. How suitable he is for the position. 
+            * Pay attention to vacancy requirements and how well the candidate fills the requirements.
+            * Analyze how well the other Human Resources agents worked, and how well their responsibilities were fulfilled.
+            * Write a general report on the Human Resources Agents work.
+            * If there are improvements to be made, clarify them professionally and detailed.
+              """,
+        backstory="""
+              You are an extremely professional, very experienced  Senior Human Resources General Manager. 
+              You have 25+ years of experience in job interviews and resume reviewing. 
+              Your responsibility is to control the work of all Human Resources Agents.
+              How well the task are being executed. 
+              What should be improved, what should be preserved.
+              """,
+        allow_delegation=False,
+        verbose=True,
+        llm=open_llm
     )
-    answer = task.execute()
-    with st.chat_message("assistant"):
-        st.write(answer)
+
+
+# Tasks #
+##############################################
+def cv_analysis_task(vacancy, resume):
+    return Task(
+        description=f"""
+            * Read this information about the vacancy: {vacancy} \n\n.
+            * Read this contents of a resume: {resume} \n\n.
+            * Do not improvise,  do not make conclusions if there is not enough information. 
+            * Just work with real facts and information.
+            * The goal is to analyze the given 'RESUME', and write a 'First analysis report'.
+            * Search in the resume and find the matching information for each "Vacancy Requirements" and "would be plus" item
+            * Rate from 0% to 100% the information found in the resume. 
+            * 0% = No information found, 
+            * 100% = The information in the resume is 100% guarantees that the requirement/woule be plus was met.  
+            * "Work Experience" must be detailed at least for one last place of employment. ie: Company name, start-end dates, references and etc.
+            * Do not rate "Work Experience" just by words and sentences. Seek for detailed information such as: company name, start-end dates, references and etc.
+            * Foreign language level must be referenced in resume to receive more  than 80%. ie: English Level: C+ ,Advanced, courses, diplomas and etc.
+            * Do not rate foreign language level by the resume contains. Seek for detailed information.
+            * Use the "expected output" for reference. 
+           """,
+        expected_output=f"""
+        Requirement: Name of requirement in vacancy details (% Your analysis in percentage) \n
+        \t Reason: Explain your grounds. Why you gave that rating; \n\n
+        
+        Would be plus: Name of Would be plus in vacancy details (% Your analysis in percentage) \n
+        \t Reason: Explain your grounds. Why you gave that rating; \n\n
+        """,
+        agent=cv_analyst()
+    )
+
+
+# Set Defaults
+current_step = st.session_state.get('current_step', 1)
+responses = st.session_state.get('responses', {})
+selected_vacancy_value = responses.get("selected_vacancy", "")
+vacancy_names = GetVacanciesData().get_vacancy_names()
+index = None if not selected_vacancy_value or selected_vacancy_value not in vacancy_names else vacancy_names.index(
+    selected_vacancy_value)
+
+
+if current_step == 1:
+    # The form
+    responses["name"] = st.text_input("Name", value=responses.get("name", ""), key=f"name")
+    responses["selected_vacancy"] = st.selectbox('Select a vacancy', vacancy_names, index=index, key="selected_vacancy")
+    selected_vacancy = responses["selected_vacancy"]
+    uploaded_file = st.file_uploader("Choose a file")
+    # Buttons
+    col1, col2 = st.columns([14, 3])
+    # Initialize the form
+    if col2.button("Next"):
+        if are_all_fields_filled(responses) and uploaded_file:
+            # Set Session responses
+            st.session_state['responses'] = responses
+            # Loader
+            with st.spinner("The assistant is analyzing the details of your CV, please wait..."):
+                # Set the temp path
+                temp_path = get_temp_path()
+                # Read the resume file
+                if uploaded_file is not None:
+                    cv_contents = check_uploaded_file(uploaded_file)  # operation that takes time
+                    with open(os.path.join(temp_path, "resume.txt"), "w") as f:
+                        f.write(cv_contents)
+                # Get the selected vacancy details
+                vacancy_info = GetVacanciesData().get_vacancy_info(selected_vacancy)
+                if vacancy_info:
+                    vacancy_details = f"Vacancy Name: {vacancy_info['name']} \n" \
+                                      f"Vacancy Requirements Affect: {vacancy_info['requirements_affect']}\n" \
+                                      f"Vacancy Requirements: \n {vacancy_info['requirements']}\n\n" \
+                                      f"Vacancy Would be Plus Affect: {vacancy_info['would_be_plus_affect']} \n" \
+                                      f"Vacancy Would be Plus: \n {vacancy_info['would_be_plus']} \n" \
+
+                    with open(os.path.join(temp_path, "vacancy.txt"), "w") as f:
+                        f.write(vacancy_details)
+                else:
+                    vacancy_details = "Vacancy not found"
+
+        else:
+            st.warning("All fields are required. Please check the fields for errors.")
---
+++ tools/getvacancies.py
@@ -0,0 +1,52 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+class GetVacanciesData():
+
+    def __init__(self):
+        # Define the scope and credentials
+        scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name('cv-analyzer-421719-f8a9b596b0db.json', scope)
+
+        # Authorize the client
+        client = gspread.authorize(credentials)
+
+        # Open the Google Sheet by its URL
+        sheet_url = 'https://docs.google.com/spreadsheets/d/14r4OdQPCmkvKk1pdSeFi_IKVmtU5iGpYGypIv5itnec/edit#gid=0'
+        self.sheet = client.open_by_url(sheet_url)
+
+
+    def get_vacancy_names(self):
+        # Get the first sheet in the workbook
+        worksheet = self.sheet.sheet1
+
+        # Get all values in the sheet
+        vacancies = worksheet.get_all_values()
+
+        # Loop through each row (excluding the header) and retrieve the information
+        vacancy_names = [vacancy[0] for vacancy in vacancies[1:]]  # Assuming vacancy name is in the first column (index 0)
+        return vacancy_names
+
+    def get_vacancy_info(self, vacancy_name):
+        # Get the first sheet in the workbook
+        worksheet = self.sheet.sheet1
+
+        # Get all values in the sheet
+        vacancies = worksheet.get_all_values()
+
+        # Find the row corresponding to the selected vacancy
+        for vacancy in vacancies[1:]:
+            if vacancy[0] == vacancy_name:  # Vacancy name is in the first column (index 0)
+                vacancy_info = {
+                    'name': vacancy[0],
+                    'requirements': vacancy[1],
+                    'requirements_affect': vacancy[2],
+                    'would_be_plus': vacancy[3],
+                    'would_be_plus_affect': vacancy[4]
+                }
+
+                return vacancy_info
+
+        return None  # Return None if vacancy not found
